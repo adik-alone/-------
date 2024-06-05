8fe32f40de6ad346b8e4e30979db2b2074f71d48
uranus destroyer <mr.endder55t@mail.ru>
2024-06-06 01:22:00 +0300

    commit

--- lab2/task1.py
+++ lab2/task1.py
@@ -6,27 +6,36 @@ n1, n2 = 25, 25
 mu1, mu2 = 0, 0
 sigma1, sigma2 = 2, 1
 alpha = 0.05
+total = []
 
-# Генерация выборок
-# Генерирует массив чисел, распределенных по нормальному закону
-X1 = np.random.normal(mu1, np.sqrt(sigma1), n1)
-X2 = np.random.normal(mu2, np.sqrt(sigma2), n2)
+for i in range(1000):
+    # Генерация выборок
+    # Генерирует массив чисел, распределенных по нормальному закону
+    X1 = np.random.normal(mu1, sigma1, n1)
+    X2 = np.random.normal(mu2, sigma2, n2)
 
-# Выборочные дисперсии
-# ddof=1, чтобы получить несмещённую оценку дисперсии (деление на n-1)
-S1 = np.var(X1)
-S2 = np.var(X2)
+    # Выборочные дисперсии
+    S1 = sum(X1 ** 2)
+    S2 = sum(X2 ** 2)
 
-# Оценка отношения дисперсий
-tau_hat = (n2 * S1) / (n1 * S2)
-print(f"Оценка отношения дисперсий: {tau_hat}")
+    # Оценка отношения дисперсий
+    tau = (n2 * S1) / (n1 * S2)
+    print(f"Оценка отношения дисперсий: {tau}")
 
-# Критические значения распределения Фишера
-F_low = stats.f.ppf(alpha / 2, dfn=n1-1, dfd=n2-1)
-F_high = stats.f.ppf(1 - alpha / 2, dfn=n1-1, dfd=n2-1)
+    # Критические значения распределения Фишера
+    F_low = stats.f.ppf(alpha / 2, dfn=n1 - 1, dfd=n2 - 1)
+    F_high = stats.f.ppf(1 - alpha / 2, dfn=n1 - 1, dfd=n2 - 1)
+    print(f"F_low - {F_low}, F_high - {F_high}")
 
-# Доверительный интервал
-CI_low = tau_hat / F_high
-CI_high = tau_hat / F_low
+    # Доверительный интервал
+    CI_low = tau / F_high
+    CI_high = tau / F_low
 
-print(f"Доверительный интервал для отношения дисперсий: ({CI_low}, {CI_high})")
+    if CI_high >= tau >= CI_low:
+        total.append(1)
+    else:
+        total.append(0)
+
+    print(f"Доверительный интервал для отношения дисперсий: ({CI_low}, {CI_high})")
+
+print(sum(total))
