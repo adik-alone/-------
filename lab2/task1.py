85ed85991ebe75beb6349972c2cb3e051b869595
uranus destroyer <mr.endder55t@mail.ru>
2024-06-06 12:45:19 +0300

    task 1 and task 2 for lab2

--- lab2/task1.py
+++ lab2/task1.py
@@ -2,38 +2,45 @@ import numpy as np
 import scipy.stats as stats
 
 # Параметры задачи
-n1, n2 = 10000, 10000
 mu1, mu2 = 0, 0
 sigma1, sigma2 = 2, 1
 alpha = 0.05
-total = 0
-probability = 0
 
 
-for i in range(1000):
-    # генерация выборок
-    X1 = np.random.normal(mu1, np.sqrt(sigma1), n1)
-    X2 = np.random.normal(mu2, np.sqrt(sigma2), n2)
+def sol(n1, n2, mu1, mu2, sigma1, sigma2, alpha):
+    total = 0
+    probability = 0
+    for i in range(1000):
+        # генерация выборок
+        X1 = np.random.normal(mu1, np.sqrt(sigma1), n1)
+        X2 = np.random.normal(mu2, np.sqrt(sigma2), n2)
 
-    # выборочные дисперсии
-    S1 = np.var(X1, ddof=1)
-    S2 = np.var(X2, ddof=1)
+        # выборочные дисперсии
+        S1 = np.var(X1, ddof=1)
+        S2 = np.var(X2, ddof=1)
 
-    tau = S1 / S2
-    tau_real = sigma1 / sigma2
+        tau = S1 / S2
+        tau_real = sigma1 / sigma2
 
-    # криты распределения фишера
-    F_low = stats.f.ppf(alpha / 2, n1 - 1, n2 - 1)
-    F_high = stats.f.ppf(1 - alpha / 2, n1 - 1, n2 - 1)
+        # криты распределения фишера
+        F_low = stats.f.ppf(alpha / 2, n1 - 1, n2 - 1)
+        F_high = stats.f.ppf(1 - alpha / 2, n1 - 1, n2 - 1)
 
-    # доверительный интервал
-    CI_low = tau / F_high
-    CI_high = tau / F_low
+        # доверительный интервал
+        CI_low = tau / F_high
+        CI_high = tau / F_low
 
-    if CI_low <= tau_real <= CI_high:
-        total += 1
+        if CI_low <= tau_real <= CI_high:
+            total += 1
 
     probability = total / 1000
+    return total, probability
 
 
-print(total, probability)
+total1, probability1 = sol(25, 25, mu1, mu2, sigma1, sigma2, alpha)
+print(f"объем выборки 25:")
+print(f"попавшие - {total1}, точность - {probability1}")
+
+total1, probability1 = sol(10000, 10000, mu1, mu2, sigma1, sigma2, alpha)
+print(f"объем выборки 10к:")
+print(f"попавшие - {total1}, точность - {probability1}")
